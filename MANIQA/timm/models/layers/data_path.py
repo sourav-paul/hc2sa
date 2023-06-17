ec21233e522d6714265b8c7c684939455307ed99
Sourav Paul Roman <srvpul@gmail.com>
2023-06-17 03:50:54 +0200

    HyperIQA, KonCept512, MANIQA

---
+++ MANIQA/timm/models/layers/data_path.py
@@ -0,0 +1,66 @@
+# datasets' directories
+
+path = '/home/ec2-user/SageMaker/AmazonSageMaker-AdsQuality/data/'
+
+#=================BAPPS==========================
+
+baaps             = path + 'data/baaps/'
+
+#=================CID-IQ / CIS-IQ==========================
+
+cidiq                   = path + 'cid-iq/'
+
+cidiq_reproduction_jpeg = path + 'cid-iq/Images/Reproduction/2_JPEG_Compression/'
+
+#=================KADID 10K==========================
+
+kadid10k          = path + 'kadid_10k/'
+
+#=================KADIS 700K==========================
+
+kadis700k         = path + 'kadis_700k/'
+
+#=================KONIQ 10K==========================
+
+koniq10k          = path + 'koniq_10k/'
+koniq10k_1024x768 = path + 'koniq_10k/1024x768/'
+koniq10k_512x384  = path + 'koniq_10k/512x384/'
+
+#=================CLIVE==========================
+
+clive             = path + 'live-in-the-wild/'
+clive_images      = path + 'live-in-the-wild/Images/'
+
+#=================Live1==========================
+
+live1             = path + 'live-r1/'
+
+#=================LIVE2==========================
+
+live2             = path + 'live-r2'
+
+#=================Pie-App==========================
+
+pieapp            = path + 'pie_app'
+
+#=================PIPAL==========================
+
+pipal             = path + 'pipal/'
+pipal_dis_1       = path + 'pipal/Distortion_1'
+pipal_dis_2       = path + 'pipal/Distortion_2'
+pipal_dis_3       = path + 'pipal/Distortion_3'
+pipal_dis_4       = path + 'pipal/Distortion_4'
+
+pipal_dis         = path + 'pipal/Dis'
+pipal_ref         = path + 'pipal/Ref'
+
+pipal_train_label = path + 'pipal/Train_Label'
+pipal_train_ref   = path + 'pipal/Train_Ref'
+
+#=================TID2013==========================
+
+tid2013           = path + 'tid2013/'
+
+#=================Waterloo==========================
+
+waterloo          = path + 'waterloo/'
